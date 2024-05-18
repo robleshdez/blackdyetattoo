c7aa849bd8f9784903b064c601c1a94d1d12f46c
Rolando Robles Hernández <robleshdez@gmail.com>
2024-05-18 12:22:07 -0400

    logo

--- js/custom.js
+++ js/custom.js
@@ -63,10 +63,10 @@ $(function () {
             logo = $(".navbar .logo> img");
         if (bodyScroll > 100) {
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
+            logo.attr('src', 'img/logo.png');
         } else {
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'img/logo-light.png');
+            logo.attr('src', 'img/logo.png');
         }
     });
     // close navbar-collapse when a  clicked
